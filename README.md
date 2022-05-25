b5bb42dd0c08bd49df93dd808413dd40175aba45
RegineaW <73669527+RegineaW@users.noreply.github.com>
2022-05-25 18:26:56 -0400

    Setting up finale cake result

--- README.md
+++ README.md
@@ -20,3 +20,5 @@ Resources: https://codesandbox.io/s/trivia-project-regineaw-4qedg?file=/src/comp
 Day 2 : 5/4/22 - "Last class I planned out my project. Fot this class I hope to get started and hopefully get to a good distance. Getting a title and the questions of my games down"
 
 Day 3: 5/11/22 - A little after class I got the link for my images. Today I would like to have me questions mapped as well as my photos and my submit button going.
+
+Day 4: 5/18/22 - Got quite a bit done today. We made the buttons clickable and for it to go on to the next question. We also made a demo ending page, which we are currently still working on, and I created another file to hold all the cake results depending the users choice. But other than that, it is almost done.
--- src/App.js
+++ src/App.js
@@ -1,60 +1,78 @@
 import "./styles.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import data from "./data-file.json";
+import _ from "lodash";
+import cakes from "./cakes.json";
 
 function Question(props) {
   return <div>{props.question}</div>;
 }
 
 function Answers(props) {
-  // const [resultCake, setResultCake] = useState([]);
-  function handleNextQuestion(index) {
-    // console.log(resultCake);
-    // let newArr = resultCake.concat(index);
-    // setResultCake(newArr);
-    // console.log(resultCake);
-    props.setCurrentQuestion(props.currentQuestion + 1);
+  function handleNextQuestion(e) {
+    // Adding each index (e.target.id) to the result cake array
+    props.setResultCake((currentResultCake) => {
+      let newArr = [...currentResultCake, parseInt(e.target.id, 10)];
+      return newArr;
+    });
+
+    props.setCurrentQuestion((currentQuestion) => currentQuestion + 1);
   }
   return props.answers.map(function (pic, index) {
     return (
-      <button>
-        <img src={pic} alt="cake" onClick={handleNextQuestion} />
+      <button key={index}>
+        <img
+          id={index}
+          src={pic}
+          alt="cake"
+          onClick={(e) => handleNextQuestion(e)}
+        />
       </button>
     );
   });
 }
 
 function ResultPage(props) {
-  return <div> final cake </div>;
+  const [finalCake, setFinalCake] = useState("");
+  // 1. Iterate over cakes array
+  // 2. Compare if current cake choices arr == resultCake
+  for (let i = 0; i < cakes.length; i++) {
+    if (_.isEqual(props.resultCake, cakes[i].choices)) {
+      console.log(cakes[i].image);
+      // 3. If equal, add image url to a state var
+      setFinalCake(cakes[i].image);
+      console.log("here");
+    }
+  }
+
+  // 4. Print image url src as the state var
+
+  return (
+    <div>
+      final cake
+      <img src={finalCake} alt="finalCake" />
+    </div>
+  );
 }
 
 export default function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [gameOver, setGameOver] = useState(false);
-
-  useEffect(() => {
-    if (currentQuestion + 1 > data.length) {
-      // console.log(currentQuestion);
-      // console.log(data.length);
-      setGameOver(true);
-    }
-  }, [currentQuestion]);
-
+  const [resultCake, setResultCake] = useState([]);
   return (
     <div className="App">
-      {!gameOver ? (
+      {currentQuestion < data.length ? (
         <div>
           <h1>Welcome To Cake Maker</h1>
           <Question question={data[currentQuestion].question.text} />
           <Answers
             answers={data[currentQuestion].question.choices}
-            currentQuestion={currentQuestion}
             setCurrentQuestion={setCurrentQuestion}
+            setResultCake={setResultCake}
           />
         </div>
       ) : (
         <div>
-          <ResultPage />
+          <ResultPage resultCake={resultCake} />
         </div>
       )}
     </div>
